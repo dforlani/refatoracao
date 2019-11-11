52890966316d87db496d4296f7e9acbab17b498e
aluno <dforlani@gmail.com>
2019-11-11 19:52:41 -0300

    Refactoring 3: Move Method: Passo 1

--- Refatoracao/JavaApplication1/src/javaapplication1/Rental.java
+++ Refatoracao/JavaApplication1/src/javaapplication1/Rental.java
@@ -10,17 +10,43 @@ package javaapplication1;
  * @author aluno
  */
 class Rental {
+
     private Movie _movie;
     private int _daysRented;
 
     public Rental(Movie movie, int daysRented) {
-      _movie = movie;
-      _daysRented = daysRented;
+        _movie = movie;
+        _daysRented = daysRented;
     }
+
     public int getDaysRented() {
-      return _daysRented;
+        return _daysRented;
     }
+
     public Movie getMovie() {
-      return _movie;
+        return _movie;
+    }
+
+    public double getCharge() {
+        double thisAmount = 0;
+        //determine amounts for each line
+        switch (this.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (this.getDaysRented() > 2) {
+                    thisAmount += (this.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (this.getDaysRented() > 3) {
+                    thisAmount += (this.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
     }
 }
