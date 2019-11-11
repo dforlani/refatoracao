2792969e798d9fe81045675273ec866cefbaa872
aluno <dforlani@gmail.com>
2019-11-11 20:16:52 -0300

    Refactoring 5: Extract Method Tinha faltado uma parte

--- Refatoracao/JavaApplication1/src/javaapplication1/Customer.java
+++ Refatoracao/JavaApplication1/src/javaapplication1/Customer.java
@@ -47,12 +47,7 @@ class Customer {
             // add frequent renter points
              frequentRenterPoints += each.getFrequentRenterPoints();
              
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-                    && each.getDaysRented() > 1) {
-                frequentRenterPoints++;
-            }
-
+           
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t"
                     + String.valueOf(each.getCharge()) + "\n";
