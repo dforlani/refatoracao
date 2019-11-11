52fc305009d173c11bdf4db65c0388c535528d9a
aluno <dforlani@gmail.com>
2019-11-11 20:38:20 -0300

    Refactoring 7: Replace Conditional with Polymorphism: mover getFrequentRenterPoints de Movie para Price

--- Refatoracao/JavaApplication1/src/javaapplication1/Price.java
+++ Refatoracao/JavaApplication1/src/javaapplication1/Price.java
@@ -12,7 +12,14 @@ package javaapplication1;
 abstract class Price {
 
     abstract int getPriceCode();
+
     abstract double getCharge(int daysRented);
 
-  
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
